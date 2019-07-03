0058e8dc560551d6166ea7c3da70161fe0d6c8d6
tig4605246 <tig4605246@gmail.com>
2019-07-03 19:44:25 +0200

    First commit

---
+++ mybot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+ 
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+ 
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+  if (message.content.startsWith("神經病")) {
+    message.channel.send("什麼鬼套路");
+  }
+});
+
+client.on("message", (message) => {
+    if (message.content.startsWith("神經病")) {
+      message.channel.send("什麼鬼套路");
+    }
+});
+
+client.on("message", (message) => {
+    if (message.content.includes("POE")||message.content.includes("流亡黯道")) {
+      message.channel.send("垃圾雞舍");
+    }
+});
+ 
+client.login("NTk2MDEyMjUyMTE3ODYwMzgx.XRzc2A.CeHlxViPJmzrcphb6NwcZwXpnRQ");
